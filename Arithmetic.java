bc3e94b4668e0f659bf9b77d2ac5bca708d75097
MAKIN <mohammedmakin10@gmail.com>
2026-03-11 09:39:01 +0530

    Fix string concatenation in Arithmetic class

--- Arithmetic.java
+++ Arithmetic.java
@@ -2,9 +2,9 @@ class Arithmetic{
   public static void main(String args[]){
     int a=10;
     int b=10;
-    System.out.println("Addition="a+b);
-    System.out.println("Subtraction="a-b);
-    System.out.println("Devision="a/b);
-    System.out.println("Multiplication="a*b);
+    System.out.println("Addition="+a+b);
+    System.out.println("Subtraction="+a-b);
+    System.out.println("Devision="+a/b);
+    System.out.println("Multiplication="+a*b);
   }
 }
