4684c443ec2ca0dbd95a5a2273c7ec733333b782
MAKIN <mohammedmakin10@gmail.com>
2026-03-11 09:37:07 +0530

    Implement basic arithmetic operations in Arithmetic.java

---
+++ Arithmetic.java
@@ -0,0 +1,10 @@
+class Arithmetic{
+  public static void main(String args[]){
+    int a=10;
+    int b=10;
+    System.out.println("Addition="a+b);
+    System.out.println("Subtraction="a-b);
+    System.out.println("Devision="a/b);
+    System.out.println("Multiplication="a*b);
+  }
+}
